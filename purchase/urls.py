441577d1053ee7de2e8d214e4ba6e8f51e2aaef8
JatinWadhwani26 <wadhwanijatin26@gmail.com>
2024-09-19 23:55:22 +0530

    adding last purchase rate and mrp in purchase form

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,6 +1,6 @@
 # purchase/urls.py
 from django.urls import path
-from .views import purchase_list, purchase_form
+from .views import purchase_list, purchase_form, get_purchase_item_data
 
 urlpatterns = [
     path('create', purchase_form, name='create_purchase'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('purchase_list', purchase_list, name='purchase_list'),
 
     path('edit/<int:id>', purchase_form, name='edit_purchase'),
+
+    path('get_purchase_item_data/<int:item_id>/', get_purchase_item_data, name='get_purchase_item_data'),
 ]
